49267600e30770d33d15b5086800f13d35a030db
Birkegard <birkegard@hotmail.se>
2026-01-27 10:35:30 +0100

    Lagt till OrderItem controller, värden i orderitem och skapat en html.

--- src/main/java/se/iths/groupspring/model/OrderItem.java
+++ src/main/java/se/iths/groupspring/model/OrderItem.java
@@ -1,4 +1,50 @@
 package se.iths.groupspring.model;
 
+
 public class OrderItem {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public OrderItem(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public OrderItem() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", name=" + name +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
