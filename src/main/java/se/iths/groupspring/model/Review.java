9a94b97aff352d0f8b16cb9c66a5a55ac93a576d
conniekarlzen <conniekarlzen@gmail.com>
2026-01-27 12:57:09 +0100

    Revert "/lagt till Person, Review och Payment samt Controllers"
    This reverts commit 4bb351f6452dc9fd5acc40d0e348a4493f15d4fc.

--- src/main/java/se/iths/groupspring/model/Review.java
+++ src/main/java/se/iths/groupspring/model/Review.java
@@ -1,46 +1,4 @@
 package se.iths.groupspring.model;
 
 public class Review {
-    private Long id;
-    private int rating;
-    private String comment;
-
-    public Review(Long id, int rating, String comment) {
-        this.id = id;
-        this.rating = rating;
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Review{" +
-                "id=" + id +
-                ", rating=" + rating +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
 }
