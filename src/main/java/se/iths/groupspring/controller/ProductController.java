f9872fbd087b1c11923b26181bc58e2a502d8fde
conniekarlzen <conniekarlzen@gmail.com>
2026-01-27 13:18:15 +0100

    Added product model, product controller and html for product

---
+++ src/main/java/se/iths/groupspring/controller/ProductController.java
@@ -0,0 +1,28 @@
+package se.iths.groupspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.groupspring.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(Model model) {
+
+        List<Product> products = new ArrayList<>();
+
+        products.add(new Product(1L, "iPhone", "Smartphone från Apple", 12999.0));
+        products.add(new Product(2L, "Samsung TV", "55 tum Smart TV", 8999.0));
+        products.add(new Product(3L, "Bluetooth-högtalare", "Trådlös högtalare", 999.0));
+
+        model.addAttribute("products", products);
+
+        return "products";
+    }
+}
+
