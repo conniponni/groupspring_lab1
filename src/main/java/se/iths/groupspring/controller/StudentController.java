449e74c96ae11e83974dcc5c4f0043f93e979688
conniekarlzen <conniekarlzen@gmail.com>
2026-01-27 13:46:44 +0100

    Request mapping for studentcontroller

--- src/main/java/se/iths/groupspring/controller/StudentController.java
+++ src/main/java/se/iths/groupspring/controller/StudentController.java
@@ -3,15 +3,17 @@ package se.iths.groupspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.groupspring.model.Student;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/students")
 public class StudentController {
 
-    @GetMapping("/students")
+    @GetMapping
     public String getStudents(Model model) {
 
         List<Student> students = new ArrayList<>();
